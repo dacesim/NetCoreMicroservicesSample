eb5e4ac431e7478465ebf036e20954ff384e6165
dacesim <danielacesim@gmail.com>
2019-10-30 21:00:07 +0100

    Using Convey

--- Src/UsersService/UsersService/Program.cs
+++ Src/UsersService/UsersService/Program.cs
@@ -1,17 +1,29 @@
 using Convey;
+using Convey.CQRS.Commands;
+using Convey.CQRS.Events;
+using Convey.CQRS.Queries;
 using Convey.Discovery.Consul;
 using Convey.LoadBalancing.Fabio;
 using Convey.Logging;
+using Convey.MessageBrokers.CQRS;
+using Convey.MessageBrokers.RabbitMQ;
 using Convey.Metrics.AppMetrics;
+using Convey.Persistence.Redis;
 using Convey.Tracing.Jaeger;
+using Convey.Tracing.Jaeger.RabbitMQ;
 using Convey.WebApi;
+using Convey.WebApi.CQRS;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
+using Users.Service.Commands;
+using Users.Service.Queries;
+using static Users.Service.Events.UserCreated;
 
-namespace APIGraphQL
+namespace Users.Service
 {
     public class Program
     {
@@ -26,6 +38,14 @@ namespace APIGraphQL
                     .AddConsul()
                     .AddFabio()
                     .AddJaeger()
+                    .AddCommandHandlers()
+                    .AddEventHandlers()
+                    .AddQueryHandlers()
+                    .AddInMemoryCommandDispatcher()
+                    .AddInMemoryEventDispatcher()
+                    .AddInMemoryQueryDispatcher()
+                    .AddRedis()
+                    .AddRabbitMq(plugins: p => p.AddJaegerRabbitMqPlugin())
                     .AddMetrics()
                     .AddWebApi()
                     .Build())
@@ -33,9 +53,15 @@ namespace APIGraphQL
                     .UseErrorHandler()
                     .UseInitializers()
                     .UseRouting()
+                    .UseDispatcherEndpoints(endpoints => endpoints
+                        .Get("ping", ctx => ctx.Response.WriteAsync("OK"))
+                        .Get<GetUser.Query, GetUser.Result>("users/{userId}")
+                        .Post<CreateUser.Command>("users", afterDispatch: (cmd, ctx) => ctx.Response.Created($"users/{cmd.UserId}")))
                     .UseJaeger()
                     .UseInitializers()
-                    .UseMetrics())
+                    .UseMetrics()
+                    .UseRabbitMq()
+                    .SubscribeEvent<UserCreatedEvent>())
                 .UseLogging();
             });
 
